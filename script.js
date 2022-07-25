a15043773faa4f6925602e0c10c796928a9a3512
Jonnevie <56157123+Jonnevie@users.noreply.github.com>
2022-07-25 23:28:49 +1000

    added folder for archive of answers

--- script.js
+++ script.js
@@ -1,19 +1,2 @@
-// WARM UP DIAGONAL DIFFERENCE
 
-let arr = [[10,2,4,5],[4,5,6,10],[10,8,-12,5],[4,5,6,10]];
-let n = arr.length;
-let output = 0;
-let primaryDiagonal = 0;
-let secondaryDiagonal = 0;
-
-function diagonalDifference(){
-    for (let i=0; i<arr.length; i++){
-   primaryDiagonal += (arr[i][i]);
-   secondaryDiagonal += (arr[i][(n-1)-i]);
-}
-output = Math.abs(primaryDiagonal - secondaryDiagonal)
-return output;
-}
-
-diagonalDifference()
 
